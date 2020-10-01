50826f4a835b7ed3b747125e70dd59110218eb6e
vipscodes <vipscodes@gmail.com>
2020-10-01 19:48:31 +0700

    View

---
+++ app/controllers/About.php
@@ -0,0 +1,14 @@
+<?php
+
+class About
+{
+    public function index($nama = 'Avip', $pekerjaan = 'Mahasiswa')
+    {
+        echo "Hallo, nama saya $nama, saya adalah seorang $pekerjaan";
+    }
+
+    public function page()
+    {
+        echo 'About/page';
+    }
+}
