41572a9b939f691108fdf8c5e01ed5b98f5dc1fe
vipscodes <vipscodes@gmail.com>
2020-10-02 22:10:52 +0700

    Delete Data

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -58,4 +58,15 @@ class Mahasiswa_model
 
         return $this->db->rowCount();
     }
+
+    public function hapusDataMahasiswa($id)
+    {
+        $query = "DELETE FROM mahasiswa_1 WHERE id = :id";
+        $this->db->query($query);
+        $this->db->bind('id', $id);
+
+        $this->db->execute();
+
+        return $this->db->rowCount();
+    }
 }
