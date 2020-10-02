87a55606e9e389b8ca415bbea50384ec6cc7d74e
vipscodes <vipscodes@gmail.com>
2020-10-02 21:17:08 +0700

    Database Wrapper

--- app/core/Constants.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-define('BASEURL', 'http://localhost/semester5/rekweb2020_183040024_phpmvc/public');
--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -23,25 +23,26 @@ class Mahasiswa_model
     //     ],
     // ];
 
-    private $dbh; // database handler
-    private $stmt;
+
+    private $table = 'mahasiswa_1';
+    private $db;
 
     public function __construct()
     {
-        //data source name
-        $dsn = 'mysql:host=localhost;dbname=phpmvc';
-
-        try {
-            $this->dbh = new PDO($dsn, 'root', '');
-        } catch (PDOException $e) {
-            die($e->getMessage());
-        }
+        $this->db = new Database;
     }
+
 
     public function getAllMahasiswa()
     {
-        $this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa_1');
-        $this->stmt->execute();
-        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+        $this->db->query('SELECT * FROM ' . $this->table);
+        return $this->db->resultSet();
+    }
+
+    public function getMahasiswaById($id)
+    {
+        $this->db->query('SELECT * FROM ' . $this->table . ' WHERE id=:id');
+        $this->db->bind('id', $id);
+        return $this->db->single();
     }
 }
