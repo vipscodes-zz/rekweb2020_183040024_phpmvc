6bd8974b3ca73850f68c533911f5081849b45d23
vipscodes <vipscodes@gmail.com>
2020-10-02 20:02:01 +0700

    Assets

---
+++ app/core/Constants.php
@@ -0,0 +1,3 @@
+<?php
+
+define('BASEURL', 'http://localhost/semester5/rekweb2020_183040024_phpmvc/public');
--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,6 +1,9 @@
-<h1>About Me</h1>
-<p>Halo, Nama saya
-    <?= $data['nama'] ?>, umur saya
-    <?= $data['umur'] ?> tahun, saya adalah seorang
-    <?= $data['pekerjaan'] ?>
-</p>
+<div class="container">
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?= BASEURL; ?>/img/as.png" alt="VIP CODE STUDIO" width="200" class="rounded-circle shadow">
+    <p>Halo, Nama saya
+        <?= $data['nama'] ?>, umur saya
+        <?= $data['umur'] ?> tahun, saya adalah seorang
+        <?= $data['pekerjaan'] ?>
+    </p>
+</div>
